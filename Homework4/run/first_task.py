2fd50d31bee83d7c282d43d778a76f78a26ae240
Radu Stefan MIHALACHE <rmihalache@bitdefender.com>
2024-05-02 13:23:26 +0300

    som task

--- Homework4/run/first_task.py
+++ Homework4/run/first_task.py
@@ -3,11 +3,12 @@ from Homework4.data_utils import normalize
 from Homework4.first_task.som import train_som, plot_u_matrix, plot_torus, k_means
 from Homework4.metrics import get_adjusted_random_index
 from Homework4.visualize import plot_data
+from typing import Optional
 
 
-def som_on_iris():
+def som_on_iris(map_size: Optional[tuple[int, int]] = None):
     features, labels = dataset_iris()
-    som = train_som(normalize(features))
+    som = train_som(normalize(features), map_size)
     plot_u_matrix(som, title="U Matrix")
     plot_u_matrix(som, title="U Matrix with Hits", hits=True)
     clusters = k_means(som, 3)
@@ -20,3 +21,4 @@ def som_on_iris():
 
 if __name__ == "__main__":
     som_on_iris()
+    som_on_iris((15, 15))
