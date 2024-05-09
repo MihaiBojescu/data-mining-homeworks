8f0a14e65e4f95b87f61493fc11df7b0e380e3cb
Mihai Bojescu <bojescu.mihai@gmail.com>
2024-05-09 23:27:31 +0300

    Homework 4: Add clusterisation on 2d datasets for the first task

---
+++ Homework4/first_task/cluster.py
@@ -0,0 +1,76 @@
+from dataclasses import dataclass
+from sklearn.cluster import KMeans, AgglomerativeClustering
+from sklearn.mixture import GaussianMixture
+import typing as t
+import numpy as np
+import matplotlib.pyplot as plt
+from Homework4.metrics import get_adjusted_random_index
+
+
+
+@dataclass
+class Dataset:
+    title: str
+    data: np.ndarray
+    nr_of_clusters: int
+
+@dataclass
+class Cluster:
+    title: str
+    y: np.ndarray
+    y_hat: np.ndarray
+
+    @property
+    def full_title(self) -> str:
+        adjusted_random_index = get_adjusted_random_index(self.y.reshape(-1), self.y_hat.reshape(-1))
+        return f"{self.title}. ARI: {adjusted_random_index}"
+
+def run_k_means(x: list[int], nr_of_clusters: int):
+    return KMeans(
+        n_clusters=nr_of_clusters,
+        max_iter=400,
+    ).fit_predict(X=x)
+
+
+def run_em_gmm(x: list[int], nr_of_clusters: int):
+    return GaussianMixture(
+        n_components=nr_of_clusters,
+        max_iter=100,
+    ).fit_predict(X=x)
+
+
+def run_hierarchical_clustering(
+    x: list[int],
+    linkage: t.Union[
+        t.Literal["ward"],
+        t.Literal["complete"],
+        t.Literal["average"],
+        t.Literal["single"],
+    ],
+    nr_of_clusters: int,
+):
+    return AgglomerativeClustering(
+        n_clusters=nr_of_clusters, metric="euclidean", linkage=linkage
+    ).fit_predict(X=x)
+
+
+def build_cluster_plots(
+    x: np.array,
+    all_clusters: list[Cluster],
+    title: str,
+    rows=3,
+):
+    fig, axs = plt.subplots(rows, len(all_clusters) // rows)
+
+    fig.tight_layout()
+    fig.suptitle(title)
+
+    for i, cluster in enumerate(all_clusters):
+        axs[i % rows][i // rows].scatter(x[:, 0], x[:, 1], c=cluster.y_hat)
+        axs[i % rows][i // rows].set_title(cluster.full_title)
+        axs[i % rows][i // rows].set_xlabel("X")
+        axs[i % rows][i // rows].set_ylabel("Y")
+
+    fig.show()
+    fig.waitforbuttonpress()
+    plt.close()
--- Homework4/run/first_task.py
+++ Homework4/run/first_task.py
@@ -1,9 +1,96 @@
-from Homework4.data_loader import dataset_iris
+from Homework4.data_loader import (
+    dataset_iris,
+    dataset_2d_10c,
+    dataset_long,
+    dataset_order2_3clust,
+    dataset_smile,
+    dataset_square,
+)
 from Homework4.data_utils import normalize
+from Homework4.first_task.cluster import (
+    Dataset,
+    Cluster,
+    run_k_means,
+    run_em_gmm,
+    run_hierarchical_clustering,
+    build_cluster_plots,
+)
 from Homework4.first_task.som import train_som, plot_u_matrix, plot_torus, k_means
 from Homework4.metrics import get_adjusted_random_index
 from Homework4.visualize import plot_data
 from typing import Optional
+
+
+def main():
+    cluster_on_datasets()
+    # som_on_iris()
+    # som_on_iris((15, 15))
+
+
+def cluster_on_datasets():
+    datasets = [
+        Dataset(title="2d 10c", data=dataset_2d_10c(), nr_of_clusters=10),
+        Dataset(title="Long", data=dataset_long(), nr_of_clusters=2),
+        Dataset(
+            title="Order 2, 3 clusters", data=dataset_order2_3clust(), nr_of_clusters=3
+        ),
+        Dataset(title="Smile", data=dataset_smile(), nr_of_clusters=4),
+        Dataset(title="Square", data=dataset_square(), nr_of_clusters=4),
+    ]
+
+    for dataset in datasets:
+        features, labels = dataset.data
+        features = normalize(features)
+
+        clusters_k_means = run_k_means(
+            x=features, nr_of_clusters=dataset.nr_of_clusters
+        )
+        clusters_em_gmm = run_em_gmm(x=features, nr_of_clusters=dataset.nr_of_clusters)
+        clusters_hierarchical_single_linkage = run_hierarchical_clustering(
+            x=features, linkage="single", nr_of_clusters=dataset.nr_of_clusters
+        )
+        clusters_hierarchical_average_linkage = run_hierarchical_clustering(
+            x=features, linkage="average", nr_of_clusters=dataset.nr_of_clusters
+        )
+        clusters_hierarchical_complete_linkage = run_hierarchical_clustering(
+            x=features, linkage="complete", nr_of_clusters=dataset.nr_of_clusters
+        )
+        clusters_hierarchical_ward_linkage = run_hierarchical_clustering(
+            x=features, linkage="ward", nr_of_clusters=dataset.nr_of_clusters
+        )
+
+        build_cluster_plots(
+            x=features,
+            all_clusters=[
+                Cluster(title="K-Means", y=labels, y_hat=clusters_k_means),
+                Cluster(
+                    title="Expectation-Maximisation for Gaussian Mixture Models",
+                    y=labels,
+                    y_hat=clusters_em_gmm,
+                ),
+                Cluster(
+                    title="Hierarchical clustering: Single linkage",
+                    y=labels,
+                    y_hat=clusters_hierarchical_single_linkage,
+                ),
+                Cluster(
+                    title="Hierarchical clustering: Average linkage",
+                    y=labels,
+                    y_hat=clusters_hierarchical_average_linkage,
+                ),
+                Cluster(
+                    title="Hierarchical clustering: Complete linkage",
+                    y=labels,
+                    y_hat=clusters_hierarchical_complete_linkage,
+                ),
+                Cluster(
+                    title=f"Hierarchical clustering: Ward linkage",
+                    y=labels,
+                    y_hat=clusters_hierarchical_ward_linkage,
+                ),
+            ],
+            title=f"Clusters for {dataset.title}, {dataset.nr_of_clusters} clusters",
+        )
 
 
 def som_on_iris(map_size: Optional[tuple[int, int]] = None):
@@ -14,12 +101,17 @@ def som_on_iris(map_size: Optional[tuple[int, int]] = None):
     plot_u_matrix(som, title="U Matrix with Hits", hits=True)
     clusters = k_means(som, 3)
 
-    print(f"Adjusted Random Index: "
-          f"{get_adjusted_random_index(labels.reshape(-1), clusters.reshape(-1))}")
+    print(
+        f"Adjusted Random Index: "
+        f"{get_adjusted_random_index(labels.reshape(-1), clusters.reshape(-1))}"
+    )
 
-    plot_data(features, clusters, f"Clusters from K-Means with K=3 on SOM neuron weights{map_size if map_size else ''}")
+    plot_data(
+        features,
+        clusters,
+        f"Clusters from K-Means with K=3 on SOM neuron weights{map_size if map_size else ''}",
+    )
 
 
 if __name__ == "__main__":
-    som_on_iris()
-    som_on_iris((15, 15))
+    main()
