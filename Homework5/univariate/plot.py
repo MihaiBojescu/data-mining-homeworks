3ccfa60ab0ae219d85b2fb9bc4135263e36d1be6
Mihai Bojescu <bojescu.mihai@gmail.com>
2024-05-26 21:54:26 +0300

    Homework 5: Improve univariate plots

--- Homework5/univariate/plot.py
+++ Homework5/univariate/plot.py
@@ -10,28 +10,43 @@ def plot_univariate(
     outliers: np.ndarray[t.Literal["N"], float],
 ):
     inliers = data[~np.in1d(data, outliers)]
-    sturges_bins = np.ceil(1 + np.log2(len(data))).astype(int)
 
     fig = plt.figure()
     fig.suptitle(title)
-    fig.tight_layout()
 
-    gs = gridspec.GridSpec(3, 1)
+    gs = gridspec.GridSpec(5, 2, figure=fig, wspace=0.5, hspace=0.5)
 
-
-    axs1 = plt.subplot(gs[0, 0])
+    axs1 = plt.subplot(gs[0, :])
     axs1.set_title("Boxplot")
     axs1.boxplot(x=data, vert=False)
 
-    axs2 = plt.subplot(gs[1, 0])
+    axs2 = plt.subplot(gs[1, :])
     axs2.set_title("Histogram: inliers vs outliers")
     axs2.hist(x=inliers, color="blue")
     axs2.hist(x=outliers, color="red")
 
-    axs3 = plt.subplot(gs[2, 0])
+    axs3 = plt.subplot(gs[2, :])
     axs3.set_title("Plot on axis: inliers vs outliers")
     axs3.scatter(x=inliers, y=[1 for _ in range(len(inliers))], c="blue")
     axs3.scatter(x=outliers, y=[1 for _ in range(len(outliers))], c="red")
+
+    axs4 = plt.subplot(gs[3:, 0])
+    axs4.set_title("Inliers vs outliers pie plot")
+    axs4.pie(
+        x=[
+            len(inliers) / len(data),
+            len(outliers) / len(data),
+        ],
+        labels=[
+            f"Inliers ({len(inliers) / len(data) * 100:.2f}%)",
+            f"Outliers ({len(outliers) / len(data) * 100:.2f}%)",
+        ],
+        colors=["blue", "red"],
+    )
+
+    axs5 = plt.subplot(gs[3:, 1])
+    axs5.set_title("Inliers + outliers = total")
+    axs5.text(0.5, 0.5, f"{len(inliers)} + {len(outliers)} = {len(data)}", fontsize=23, ha='center')
 
     fig.show()
 
