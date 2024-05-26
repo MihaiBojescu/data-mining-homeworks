771a9a05bc9d7773bb41fe96928844a68b936830
Mihai Bojescu <bojescu.mihai@gmail.com>
2024-05-26 20:35:41 +0300

    Homework 5: Prepare first task for second task

--- Homework5/run/first_task.py
+++ Homework5/run/first_task.py
@@ -1,4 +1,5 @@
 import matplotlib.pyplot as plt
+import pandas as pd
 
 from Homework5.univariate.mean_k_sd import get_outliers_mean_k_sd
 from Homework5.univariate.k_iqr import get_outliers_k_iqr
@@ -18,8 +19,15 @@ import seaborn as sns
 import numpy as np
 
 
-def run_univariate():
+def main():
     features, labels = dataset_obesity()
+    columns = ["Weight", "Height", "Age", "FAF", "Male"]
+
+    run_univariate(features=features, labels=labels)
+    run_multivariate(features=features, labels=labels, columns=columns)
+
+
+def run_univariate(features: pd.DataFrame, labels: np.array):
     normalised_features = [normalise(features[feature]) for feature in features]
 
     for i, feature in enumerate(features):
@@ -69,13 +77,19 @@ def run_univariate():
 
 
 def __log_smooth_distribution(outlier_scores: np.array, deg: int):
-    outlier_scores = MinMaxScaler(feature_range=(1, deg)).fit_transform(outlier_scores.reshape(-1, 1))
+    outlier_scores = MinMaxScaler(feature_range=(1, deg)).fit_transform(
+        outlier_scores.reshape(-1, 1)
+    )
     outlier_scores = np.log2(outlier_scores)
-    outlier_scores = MinMaxScaler(feature_range=(0, 1)).fit_transform(outlier_scores.reshape(-1, 1))
+    outlier_scores = MinMaxScaler(feature_range=(0, 1)).fit_transform(
+        outlier_scores.reshape(-1, 1)
+    )
     return outlier_scores.reshape(-1)
 
 
-def run_isolation_forest(features: np.array, features_to_train: np.array) -> tuple[np.array, float]:
+def run_isolation_forest(
+    features: np.array, features_to_train: np.array, columns: list[str]
+) -> tuple[np.array, float]:
     isolation_forest_algorithm = IsolationForestOutlierDetector(contamination=0.05)
     isolation_forest_algorithm.build(features_to_train)
     outliers = isolation_forest_algorithm.predict(features_to_train)
@@ -85,14 +99,26 @@ def run_isolation_forest(features: np.array, features_to_train: np.array) -> tup
     outlier_scores = outliers["outlier_scores"].to_numpy()
     outlier_score_threshold = np.sort(outlier_scores)[-int(len(outlier_scores) * 0.03)]
 
-    pair_plot_6_bins(features, outlier_scores, ["Weight", "Height", "Age", "FAF", "Male"], "Isolation Forest Adjusted Scores")
+    pair_plot_6_bins(
+        features,
+        outlier_scores,
+        columns,
+        "Isolation Forest Adjusted Scores",
+    )
 
-    pair_plot_2_bins(features, (outlier_scores > outlier_score_threshold).astype(float), ["Weight", "Height", "Age", "FAF", "Male"], "Isolation Forest Outliers")
+    pair_plot_2_bins(
+        features,
+        (outlier_scores > outlier_score_threshold).astype(float),
+        columns,
+        "Isolation Forest Outliers",
+    )
 
     return outlier_scores, outlier_score_threshold
 
 
-def run_autoencoder(features: np.array, features_to_train: np.array) -> tuple[np.array, float]:
+def run_autoencoder(
+    features: np.array, features_to_train: np.array, columns: list[str]
+) -> tuple[np.array, float]:
 
     autoencoder_algorithm = AutoencoderOutlierDetector()
     autoencoder_algorithm.build(features_to_train)
@@ -103,16 +129,30 @@ def run_autoencoder(features: np.array, features_to_train: np.array) -> tuple[np
 
     smooth_outlier_scores = __log_smooth_distribution(outlier_scores, 8)
 
-    pair_plot_6_bins(features, smooth_outlier_scores, ["Weight", "Height", "Age", "FAF", "Male"], "Autoencoder Adjusted Scores")
+    pair_plot_6_bins(
+        features,
+        smooth_outlier_scores,
+        columns,
+        "Autoencoder Adjusted Scores",
+    )
 
-    pair_plot_2_bins(features, (outlier_scores > outlier_score_threshold).astype(float), ["Weight", "Height", "Age", "FAF", "Male"], "Autoencoder Outliers")
+    pair_plot_2_bins(
+        features,
+        (outlier_scores > outlier_score_threshold).astype(float),
+        columns,
+        "Autoencoder Outliers",
+    )
 
     return outlier_scores, outlier_score_threshold
 
 
-def run_local_outlier_factor(features: np.array, features_to_train: np.array) -> tuple[np.array, float]:
+def run_local_outlier_factor(
+    features: np.array, features_to_train: np.array, columns: list[str]
+) -> tuple[np.array, float]:
 
-    local_outlier_factor_algorithm = LocalOutlierFactorOutlierDetector(contamination=0.05)
+    local_outlier_factor_algorithm = LocalOutlierFactorOutlierDetector(
+        contamination=0.05
+    )
     local_outlier_factor_algorithm.build(features_to_train)
     outliers = local_outlier_factor_algorithm.predict(features_to_train)
 
@@ -121,33 +161,63 @@ def run_local_outlier_factor(features: np.array, features_to_train: np.array) ->
 
     smooth_outlier_scores = __log_smooth_distribution(outlier_scores, 64)
 
-    pair_plot_6_bins(features, smooth_outlier_scores, ["Weight", "Height", "Age", "FAF", "Male"], "Local Outlier Factor Adjusted Scores")
+    pair_plot_6_bins(
+        features,
+        smooth_outlier_scores,
+        columns,
+        "Local Outlier Factor Adjusted Scores",
+    )
 
-    pair_plot_2_bins(features, (outlier_scores > outlier_score_threshold).astype(float), ["Weight", "Height", "Age", "FAF", "Male"], "Local Outlier Factor Outliers")
+    pair_plot_2_bins(
+        features,
+        (outlier_scores > outlier_score_threshold).astype(float),
+        columns,
+        "Local Outlier Factor Outliers",
+    )
 
     return outlier_scores, outlier_score_threshold
 
 
-def run_multivariate():
-    features, labels = dataset_obesity()
+def run_multivariate(features: pd.DataFrame, labels: np.array, columns: list[str]):
     features_to_train = normalize(features.to_numpy())
 
-    isolation_forest_results, isolation_forest_threshold = run_isolation_forest(features, features_to_train)
-    autoencoder_results, autoencoder_threshold = run_autoencoder(features, features_to_train)
-    local_outlier_factor_results, local_outlier_factor_threshold = run_local_outlier_factor(features, features_to_train)
+    isolation_forest_results, isolation_forest_threshold = run_isolation_forest(
+        features, features_to_train, columns
+    )
+    autoencoder_results, autoencoder_threshold = run_autoencoder(
+        features, features_to_train, columns
+    )
+    local_outlier_factor_results, local_outlier_factor_threshold = (
+        run_local_outlier_factor(features, features_to_train, columns)
+    )
 
     combined_results = combine_predictions(
-        [isolation_forest_results > isolation_forest_threshold,
-         autoencoder_results > autoencoder_threshold,
-         local_outlier_factor_results > local_outlier_factor_threshold
-         ], 2)
+        [
+            isolation_forest_results > isolation_forest_threshold,
+            autoencoder_results > autoencoder_threshold,
+            local_outlier_factor_results > local_outlier_factor_threshold,
+        ],
+        2,
+    )
 
-    pair_plot_2_bins(features, combined_results,
-                     ["Weight", "Height", "Age", "FAF", "Male"], "Combined results outliers")
+    pair_plot_2_bins(
+        features,
+        combined_results,
+        columns,
+        "Combined results outliers",
+    )
 
     thresholds_metrics = {
-        "values": [isolation_forest_threshold, autoencoder_threshold, local_outlier_factor_threshold],
-        "names": ["Isolation forest threshold (Scaled)", "Autoencoder threshold (Scaled)", "Local outlier factor threshold (Scaled)"]
+        "values": [
+            isolation_forest_threshold,
+            autoencoder_threshold,
+            local_outlier_factor_threshold,
+        ],
+        "names": [
+            "Isolation forest threshold (Scaled)",
+            "Autoencoder threshold (Scaled)",
+            "Local outlier factor threshold (Scaled)",
+        ],
     }
 
     ax = sns.barplot(thresholds_metrics, x="names", y="values")
@@ -159,5 +229,4 @@ def run_multivariate():
 
 
 if __name__ == "__main__":
-    run_univariate()
-    run_multivariate()
+    main()
