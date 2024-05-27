19b524c846fd795e17e2476385582c4987554f17
Mihai Bojescu <bojescu.mihai@gmail.com>
2024-05-27 21:06:14 +0300

    Homework 5: Remove 1 comment

--- Homework5/src/run/second_task.py
+++ Homework5/src/run/second_task.py
@@ -31,7 +31,7 @@ def main():
     ]
     autoencoder_model_state_dict_path = "second_task_model.pt"
 
-    # univariate_outliers = run_univariate(features=features, labels=labels)
+    univariate_outliers = run_univariate(features=features, labels=labels)
     multivariate_outliers = run_multivariate(
         features=features,
         labels=labels,
